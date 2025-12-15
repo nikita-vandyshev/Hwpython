cede639c0f7fd78587a5da43e56179a79af49139
Nikita <nikvandyshev@gmail.com>
2025-12-15 07:46:43 +0300

    add test_bubble_sort_property

---
+++ tests/test_bubble_sort_property.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+from hypothesis import strategies as st
+
+from source.bubble_sort import bubble_sort
+
+
+@given(st.lists(st.integers()))
+def test_bubble_sort_matches_sorted(xs):
+    a = xs[:]          
+    bubble_sort(a)     
+    assert a == sorted(xs)
