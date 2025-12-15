76744fe275dbc070807cd13ecf1f7e27b4f2028e
Nikita <nikvandyshev@gmail.com>
2025-12-15 07:45:55 +0300

    add test_heapsort_property

---
+++ tests/heapsort/test_heapsort_property.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+from hypothesis import strategies as st
+
+from source.heapsort import heapsort
+
+
+@given(st.lists(st.integers()))
+def test_heapsort_matches_sorted(xs):
+    a = xs[:]
+    heapsort(a)          
+    assert a == sorted(xs)
